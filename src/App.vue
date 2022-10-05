0be0bedc5507267bc989d74f19414921558fde84
Mochrizaldianandakurnia <mochrizaldianandakurnia21@gmail.com>
2022-10-05 21:52:48 +0700

    First init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+the-header
+</template>
+
+<script setup>
+import TheHeader from '@/components/TheHeader.vue'
+</script>
+
+<style lang="sass">
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500;600;700&display=swap')
+
+*
+  font-family: 'Poppins', sans-serif
+  padding: 0
+  margin: 0
+  box-sizing: border-box
+
+.container
+  padding: 20px 120px
+
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+.container
+  h1 TO DO LIST APP
+</template>
+
+<style lang="sass" scoped>
+.container
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1)
+  z-index: 10
+  position: fixed
+  top: 0
+  background-color: #16ABF8
+  width: 100%
+  height: 105px
+  color: #FFF
+  display: flex
+  align-items: center
+
+  h1
+    font-size: 24px
+</style>
